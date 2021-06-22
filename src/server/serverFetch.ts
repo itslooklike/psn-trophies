2e6f0a18cbe1747fbe948e7955e5bf6d3d6e09c0
itslooklike <delsol@bk.ru>
2021-06-22 22:54:36 +0300

    add cache

--- src/server/serverFetch.ts
+++ src/server/serverFetch.ts
@@ -5,37 +5,75 @@ import { apiBaseUrl } from 'src/utils/config'
 
 const client = redis.createClient()
 const redisGet = promisify(client.get).bind(client)
-// const redisSet = promisify(client.set).bind(client)
+const redisSet = promisify(client.set).bind(client)
+const redisExp = promisify(client.expire).bind(client)
+const redisTtl = promisify(client.ttl).bind(client)
 
 let canRefresh = true
+
+const CACHE_HEADER_NAME = 'X_FROM_CACHE'
+const AUTH_HEADER_NAME = 'Authorization'
 
 export const serverFetch = axios.create({ headers: { 'Accept-Language': 'ru-RU,ru;' } })
 
 const refreshToken = () => axios.get(`${apiBaseUrl}/refresh`)
 
-serverFetch.interceptors.request.use((req) => {
-  return redisGet('token').then((token) => {
-    console.log('token from interceptor', token)
-    req.headers['Authorization'] = `Bearer ${token}`
-    delete req.headers['User-Agent']
-    return req
-  })
+serverFetch.interceptors.request.use(async (config) => {
+  if (config.baseURL) {
+    const data = await redisGet(config.baseURL)
+
+    if (data) {
+      const ttl = await redisTtl(config.baseURL)
+
+      config.adapter = function (config) {
+        return new Promise((resolve) => {
+          const res = {
+            data: JSON.parse(data),
+            status: 200,
+            statusText: 'OK',
+            headers: {},
+            config,
+            request: {},
+          }
+
+          res.config.headers[CACHE_HEADER_NAME] = ttl
+
+          console.log('💔 from cache')
+          return resolve(res)
+        })
+      }
+
+      return config
+    }
+  }
+
+  const token = await redisGet('token')
+  console.log('token from interceptor', token)
+  config.headers[AUTH_HEADER_NAME] = `Bearer ${token}`
+
+  return config
 })
 
 serverFetch.interceptors.response.use(
-  (response) => {
+  async (response) => {
+    const { baseURL, headers } = response.config
+
+    if (baseURL && !headers[CACHE_HEADER_NAME]) {
+      await redisSet(baseURL, JSON.stringify(response.data))
+      await redisExp(baseURL, 60 * 60)
+      console.log('💔 save to cache', baseURL)
+    }
+
     return response
   },
-  (error) => {
+  async (error) => {
     if (error.response.status === 401 && canRefresh) {
       canRefresh = false
 
-      return refreshToken().then(() => {
-        return redisGet('token').then((token) => {
-          error.config.headers['Authorization'] = `Bearer ${token}`
-          return serverFetch.request(error.config)
-        })
-      })
+      await refreshToken()
+      const token = await redisGet('token')
+      error.config.headers[AUTH_HEADER_NAME] = `Bearer ${token}`
+      return serverFetch.request(error.config)
     }
 
     return Promise.reject(error)

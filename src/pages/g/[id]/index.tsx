e8ec3e02ec67cbe47b31cf5173a117b5f62c4cc4
itslooklike <delsol@bk.ru>
2021-06-23 19:43:22 +0300

    update store

--- src/pages/g/[id]/index.tsx
+++ src/pages/g/[id]/index.tsx
@@ -1,3 +1,82 @@
-import { GameTrophies } from './GameTrophies'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { observer } from 'mobx-react-lite'
+import cx from 'classnames'
+import StoreGame, { ISortOptions } from 'src/store/StoreGame'
+import css from './GameTrophies.module.scss'
+
+// https://stackoverflow.com/questions/61040790/userouter-withrouter-receive-undefined-on-query-in-first-render
+
+const GameTrophies = observer(() => {
+  const [options, setOptions] = useState<ISortOptions>({ sort: 'default', filterHidden: false })
+
+  const { query } = useRouter()
+  const id = query.id as string | undefined
+
+  useEffect(() => {
+    if (id) StoreGame.fetch(id)
+  }, [id])
+
+  if (!id) return null
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.target
+    setOptions((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target
+    setOptions((prev) => ({ ...prev, [name]: checked }))
+  }
+
+  return (
+    <div>
+      <h1>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h1>
+      <div>
+        <select name="sort" value={options.sort} onChange={handleSelect}>
+          <option value="-rate">сначала самые редкие</option>
+          <option value="+rate">сначала самые популярные</option>
+          <option value="default">без сортировки</option>
+        </select>
+        <label>
+          <input
+            name="filterHidden"
+            type="checkbox"
+            checked={options.filterHidden}
+            onChange={handleInput}
+          />
+          скрыть полученные
+        </label>
+      </div>
+      <div className={css.root}>
+        {StoreGame.data[id]?.sort(options).map((trophy) => (
+          <div
+            key={trophy.trophyId}
+            className={cx(css.card, !trophy.comparedUser.earned && css.no)}
+          >
+            <img
+              width="80"
+              height="80"
+              className={css.img}
+              src={trophy.trophyIconUrl}
+              alt={trophy.trophyName}
+              loading="lazy"
+            />
+            <div>
+              <div className={css.title}>{trophy.trophyName}</div>
+              <div className={css.subtitle}>{trophy.trophyDetail}</div>
+              <div className={css.rare}>{trophy.trophyEarnedRate}%</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+})
 
 export default GameTrophies
--- src/store/StoreGame/StoreGame.ts
+++ src/store/StoreGame/StoreGame.ts
@@ -2,13 +2,40 @@ import { makeAutoObservable, runInAction } from 'mobx'
 import { clientFetch } from 'src/utils'
 import { IGameTrophies } from './types'
 
-interface IGameTrophiesStore {
-  [_: string]: IGameTrophies
-}
-
 export interface ISortOptions {
   sort?: '-rate' | '+rate' | 'default'
   filterHidden?: boolean
+}
+
+class StoreGameItem {
+  data: IGameTrophies
+
+  constructor(data: IGameTrophies) {
+    this.data = data
+    makeAutoObservable(this)
+  }
+
+  sort(options: ISortOptions = {}) {
+    const { sort, filterHidden } = options
+
+    let result = this.data.trophies
+
+    if (filterHidden) {
+      result = result.filter((trophy) => !trophy.comparedUser.earned)
+    }
+
+    if (sort === '-rate') {
+      result = [...result].sort((a, b) => +a.trophyEarnedRate - +b.trophyEarnedRate)
+    } else if (sort === '+rate') {
+      result = [...result].sort((a, b) => +b.trophyEarnedRate - +a.trophyEarnedRate)
+    }
+
+    return result
+  }
+}
+
+interface IGameTrophiesStore {
+  [_: string]: StoreGameItem
 }
 
 export class StoreGame {
@@ -22,25 +49,7 @@ export class StoreGame {
     const { data } = await clientFetch.get<IGameTrophies>(`/psn/game/${id}`)
 
     runInAction(() => {
-      this.data[id] = data
+      this.data[id] = new StoreGameItem(data)
     })
-  }
-
-  sort(id: string, options: ISortOptions = {}) {
-    const { sort, filterHidden } = options
-
-    let result = this.data[id]?.trophies
-
-    if (filterHidden) {
-      result = result.filter((trophy) => !trophy.comparedUser.earned)
-    }
-
-    if (sort === '-rate') {
-      result = [...result].sort((a, b) => +a.trophyEarnedRate - +b.trophyEarnedRate)
-    } else if (sort === '+rate') {
-      result = [...result].sort((a, b) => +b.trophyEarnedRate - +a.trophyEarnedRate)
-    }
-
-    return result
   }
 }

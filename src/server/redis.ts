51df4a70cc2d6caaf52a128bd00f43e18eb015da
itslooklike <delsol@bk.ru>
2021-11-26 16:59:15 +0300

    revert redis

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,21 +1,16 @@
-import { createClient } from 'redis'
+import redis from 'redis'
+import { promisify } from 'util'
 
-// const redisUrl = process.env.REDISTOGO_URL || ''
-const redisUrl =
-  process.env.NODE_ENV === `production`
-    ? `redis://redistogo:***REMOVED***@soapfish.redistogo.com:11809/`
-    : ``
+// const redisUrl = process.env.REDISTOGO_URL || ``
+const redisUrl = `redis://***REMOVED***@soapfish.redistogo.com:11809/`
+// const redisUrl =
+//   process.env.NODE_ENV === `production` ? `redis://***REMOVED***@soapfish.redistogo.com:11809/` : ``
 
 console.log(`👾 redisUrl: `, redisUrl)
 
-const client = createClient({
-  legacyMode: true,
-  url: redisUrl,
-})
+const client = redis.createClient(redisUrl)
 
-client.connect()
-
-export const redisGet = client.v4.get
-export const redisSet = client.v4.set
-export const redisExp = client.v4.expire
-export const redisTtl = client.v4.ttl
+export const redisGet = promisify(client.get).bind(client)
+export const redisSet = promisify(client.set).bind(client)
+export const redisExp = promisify(client.expire).bind(client)
+export const redisTtl = promisify(client.ttl).bind(client)

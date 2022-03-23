ec154c3a12c9da8629a0f856471cabae8fa65043
a.masalov <delsol@bk.ru>
2022-03-23 11:12:57 +0300

    fix login redirect

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from 'next/server'
 import { NAME_ACCOUNT_ID } from 'src/utils/config'
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
+  const { pathname, origin } = req.nextUrl
 
   const userId = req.cookies[NAME_ACCOUNT_ID]
 
@@ -11,7 +11,7 @@ export function middleware(req: NextRequest) {
   const isApiCall = pathname.startsWith(`/api`)
 
   if (pathname !== LOGIN_ROUTE && !userId && !isApiCall) {
-    return NextResponse.redirect(LOGIN_ROUTE)
+    return NextResponse.redirect(origin + LOGIN_ROUTE)
   }
 
   return NextResponse.next()

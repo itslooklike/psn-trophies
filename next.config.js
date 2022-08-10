49be3a6e86cb5bf7d4643936476d6b1f5513e584
a.masalov <delsol@bk.ru>
2022-08-10 15:49:31 +0300

    remove pwa

--- next.config.js
+++ next.config.js
@@ -1,46 +1,5 @@
-const withPWA = require(`next-pwa`)
-const runtimeCaching = require(`next-pwa/cache`)
-
-// FIXME: move to pure js
-// import { httpPsnAvatarV1, httpPsnAvatarV2 } from 'src/utils/config'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const myCache = {
-  urlPattern: ({ url }) => {
-    if (url.pathname.startsWith(`/api/psn/trophyTitles`)) return true
-    if (url.pathname.startsWith(`/api/psn/profile`)) return true
-    if (url.pathname.startsWith(`/api/psn/game`)) return true
-    if (url.pathname.startsWith(`/api/scrap`)) return true
-    return false
-  },
-  // handler: `CacheFirst`,
-  handler: `StaleWhileRevalidate`,
-  options: {
-    cacheName: `my-app-psn-apis`,
-    expiration: {
-      maxEntries: 10,
-      maxAgeSeconds: 2 * 24 * 60 * 60, // 2 days
-    },
-  },
-}
-
-const config = [
-  myCache, // что-то с ним бажит
-  ...runtimeCaching,
-]
-
-module.exports = withPWA({
+module.exports = {
   poweredByHeader: false,
-  pwa: {
-    dest: `public`,
-    // disable: process.env.NODE_ENV === `development`,
-    // INFO: херня полная это PWA, на каждом шагу подстава
-    disable: true,
-    runtimeCaching: config,
-
-    register: false,
-    skipWaiting: false,
-  },
   reactStrictMode: true,
   async rewrites() {
     return [
@@ -62,4 +21,4 @@ module.exports = withPWA({
     )
     return config
   },
-})
+}

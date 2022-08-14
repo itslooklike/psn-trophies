f3a5dc7853dd8b6ee3732e6d83ad85dd272f86d6
a.masalov <delsol@bk.ru>
2022-08-14 11:53:48 +0300

    more redirect rules

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,8 +9,10 @@ export function middleware(request: NextRequest) {
 
   const LOGIN_ROUTE = `/login`
   const isApiCall = pathname.startsWith(`/api`)
+  const isStaticCall = pathname.startsWith(`/_next`)
+  const isFileCall = pathname.includes(`.`)
 
-  if (pathname !== LOGIN_ROUTE && !userId && !isApiCall) {
+  if (pathname !== LOGIN_ROUTE && !userId && !isApiCall && !isStaticCall && !isFileCall) {
     return NextResponse.redirect(origin + LOGIN_ROUTE)
   }
 
